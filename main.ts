b855ab2cfb7c4c44a7b69963a440005e9a257570
nitosiak <105192288+nitosiak@users.noreply.github.com>
2023-10-17 20:44:54 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,7 @@
 enum RadioMessage {
     message1 = 49434
 }
-function test (mySprite: Sprite) {
-    console.log("test")
-}
+let mySprite = 0
 radio.onReceivedMessage(RadioMessage.message1, function () {
-    mySprite = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . 3 3 3 3 3 3 3 . 
-        . . . . . 3 3 3 . . . . . . . 3 
-        . . . . . 3 3 . . . . . . . 3 . 
-        . . . . 3 . 3 3 . . . . . . 3 . 
-        . . . . 3 . . 3 . . . . . . 3 . 
-        . . . 3 . . . . 3 . . . . . 3 . 
-        . . . 3 . . . . . 3 . . . . 3 . 
-        . . . 3 . . . . . . 3 3 3 . 3 . 
-        . . . 3 . . . . . . . . . 3 3 . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Player)
+    mySprite = 0
 })
-let mySprite: Sprite = null
-radio.setGroup(1)
-radio.sendMessage(RadioMessage.message1)
-namespace lol {
-    forever(function(){
-    if(controller.A.isPressed()){
-              console.log("test")
-    }
-    })
-}
-namespace say{
-    function print(message: string) {
-        console.log(message)
-    }
-    print("message")
-}
